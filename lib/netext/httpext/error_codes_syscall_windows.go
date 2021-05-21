1083ce564ad3d54f93b03b5c74892e593efb2280
Mihail Stoykov <M.Stoikov@gmail.com>
2021-05-21 12:40:48 +0300

    ...

--- lib/netext/httpext/error_codes_syscall_windows.go
+++ lib/netext/httpext/error_codes_syscall_windows.go
@@ -34,6 +34,8 @@ func getOSSyscallErrorCode(e *net.OpError, se *os.SyscallError) (errCode, string
 	switch se.Unwrap() {
 	case syscall.WSAECONNRESET:
 		return tcpResetByPeerErrorCode, fmt.Sprintf(tcpResetByPeerErrorCodeMsg, e.Op)
+	default:
+		spew.Dump(uintptr(se.Unwrap().(syscall.Errno)))
 	}
 	return 0, ""
 }
